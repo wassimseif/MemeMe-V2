28b357d7f69a0f62a13025d7497ce9d9ba5489e0
Wassim S Seifeddine <wassim_seifeddine95@live.com>
2016-03-21 20:17:21 +0200

    Implemented the delete functionality in detailViewController

--- MemeMe/MemeDetailViewController.swift
+++ MemeMe/MemeDetailViewController.swift
@@ -9,6 +9,8 @@ class MemeDetailViewController: UIViewController {
     
     var memedImage:UIImage!
     var indexOfSelectedMeme : Int?
+    var object : UIApplicationDelegate?
+    var appDelegate : AppDelegate?
     
     
     //When an image is selected this image will fill the image view of this detail view controller
@@ -16,6 +18,25 @@ class MemeDetailViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         memedImageView.image = memedImage
+       navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .Plain, target: self, action: "deleteButtonTapped")
+        object = UIApplication.sharedApplication().delegate
+        appDelegate = object as? AppDelegate
+
+    }
+    
+    func deleteButtonTapped (){
+        appDelegate?.memes.removeAtIndex(indexOfSelectedMeme!)
+        
+        if appDelegate?.memes.count != 0 {
+            navigationController?.popToRootViewControllerAnimated(true)
+            return
+            
+        }
+        
+        var memeEditorController:MemeEditorViewController
+        memeEditorController = self.storyboard?.instantiateViewControllerWithIdentifier("MemeEditorViewController") as! MemeEditorViewController
+        presentViewController(memeEditorController, animated: true, completion: nil)
+        
     }
     
 }
